3d453bc47d4ca5fedc13267a2df0fbca32a25d11
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2024-10-28 17:15:43 +0100

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -3,9 +3,12 @@ from random import randint
 
 def cloud() :
     number = randint(1,10)
-    hideturtle()
+    # hideturtle()
     pendown()
     color("#cacaca")
     for i in range(number):
+        width = randint(1,5)
         teleport(randint(-750, 750), randint(0,375))
-        forward(50*randint(1,5))
+        begin_fill()
+        forward(50*width)
+        circle(15*width,randint(10,180))
