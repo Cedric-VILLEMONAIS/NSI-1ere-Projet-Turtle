94bc73ad0bcbd2e74d3d79e94a2b344e96dfbe4e
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2024-10-28 17:34:14 +0100

    update cloud

--- cloud.py
+++ cloud.py
@@ -7,8 +7,12 @@ def cloud() :
     pendown()
     color("#cacaca")
     for i in range(number):
+        x_origin = xcor()
+        y_origin = ycor()
         width = randint(1,5)
         teleport(randint(-750, 750), randint(0,375))
         begin_fill()
         forward(50*width)
-        circle(15*width,randint(10,180))
+        while xcor() <= x_origin :
+            circle(15*width,randint(10,180))
+            right(randint(50,180))
