6ea7dd62a2580749d24357e242137d4059426d2d
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2024-10-29 15:22:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,8 @@ from tkinter import messagebox
 # On utilise round pour arrondir la valeur obtenue si l'utilisateur à donné une heure à virgule
 
 messagebox.showwarning(title="Attention", message="Pour un fonctionnement optimal veuillez utiliser une résolution d'affichage suppérieure ou égale à 1920 * 1080 pixels avec une mise à l'échelle de 100%")
-
-# user_hour = round(numinput("Heure", "Quelle heure entre 17 et 21 h ?", minval=17, maxval=21))
-# user_cloud = round(numinput("Nuages", "Est-ce que le temps est nuageux (0 non et 1 oui)?", minval=0, maxval=1))
-
-# For developpement :
-user_hour = 21
-user_cloud = 1
+user_hour = round(numinput("Heure", "Quelle heure entre 17 et 21 h ?", minval=17, maxval=21))
+user_cloud = round(numinput("Nuages", "Est-ce que le temps est nuageux (0 non et 1 oui)?", minval=0, maxval=1))
 
 # Configurer la fenêtre d'affichage
 title("NSI 1ere Projet Turtle par Nora TERRAL et Cédric VILLEMONAIS")
@@ -24,7 +19,6 @@ screensize(1500, 750)
 background_color = [17, "#caf0f8", 18, "#90e0ef", 19, "#00b4d8", 20, "#0077b6", 21, "#03045e"]
 bgcolor(background_color[(background_color.index(user_hour))+1])
 # hideturtle()
-speed(0)
 
 def star(x_start, x_end, y_start, y_end) :
     """
