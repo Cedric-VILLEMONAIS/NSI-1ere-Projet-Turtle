ab582ecfba814c49d14b02e8c7eb47bfe82dba0a
Nora123-321 <nora.terral@gmail.com>
2024-10-29 15:46:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -61,34 +61,49 @@ def sunset() :
     Cette fonction permet de dessiner un soleil et de lui attribuer une couleur en fonction de l'heure choisie précédemment
     """   
     up()
-    goto(-200,200)
-    down()
+    x=0
+    y=100
     seth(90)
     if 17>=user_hour<18:
+        x=-80
+        goto(x,y)
+        down()
         fillcolor("#FCFE43")
         begin_fill()
         color("#FCFE43")
         circle(100,180)
         end_fill()
     elif 18>=user_hour<19:
+        x=-0
+        goto(x,y)
+        down()
         fillcolor("#FFD668")
         begin_fill()
         color("#FFD668")
         circle(70,180)
         end_fill()
     elif 19>=user_hour<20:
+        x=-300
+        goto(x,y)
+        down()
         fillcolor("#FFCF68")
         begin_fill()
         color("#FFCF68")
         circle(40,180)
         end_fill()
     elif 20>=user_hour<21:
+        x=-400
+        goto(x,y)
+        down()
         fillcolor("#FFBF68")
         begin_fill()
         color("#FFBF68")
         circle(20,180)
         end_fill()
     else:
+        x=-500
+        goto(x,y)
+        down()
         fillcolor("#FF9668")
         begin_fill()
         color("#FF9668")
