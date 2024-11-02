c8585facbc5513d43aabd5332b863078e234c598
Cédric <cedric.villemonais@gmail.com>
2024-11-02 12:06:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ setup(width=1820, height=980, startx=0, starty=0)
 screensize(1500, 750)
 background_color = [17, "#caf0f8", 18, "#90e0ef", 19, "#00b4d8", 20, "#0077b6", 21, "#03045e"]
 bgcolor(background_color[(background_color.index(user_hour))+1])
-# hideturtle()
-# speed(0)
+hideturtle()
+speed(0)
 def teleport(x,y):
     penup()
     goto(x,y)
@@ -219,7 +219,7 @@ if user_cloud==1 : cloud(-750,750,0,375)
 
 if user_hour==17 or user_hour==18 : fish(-750,750,-300,0)
 if user_hour==19 or user_hour==20 : fish(-750,750,-250,0)
-if user_hour==21 : fish(-750,750,0,-200)
+if user_hour==21 : fish(-750,750,-200,0)
 
 if user_hour==17 or user_hour==18 : waves(-750,750,-300,0)
 if user_hour==19 or user_hour==20 : waves(-750,750,-250,0)
