49a5a437a69d450f75feb2be1b7479cf8c6c6b67
Nora123-321 <nora.terral@gmail.com>
2024-11-02 15:10:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ from beach import *
 
 if user_hour==20 or user_hour==21 : star(-750,750,0,375)
 sunset()
-sea()
+
+if user_hour==17 or user_hour==18 : sea(-750,750,-300,0)
+elif user_hour==19 or user_hour==20 : sea(-750,750,-250,0)
+elif user_hour==21 : sea(-750,750,-200,0)
+
 if user_cloud==1 : cloud(-750,750,0,375)
 
 if user_hour==17 or user_hour==18 : fish(-750,750,-300,0)
