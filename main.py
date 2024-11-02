cb3ce9e629b35add2f93f93eca8f2255406be1f0
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2024-11-02 15:30:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ if user_hour==17 or user_hour==18 : sea(-750,750,-300,0)
 elif user_hour==19 or user_hour==20 : sea(-750,750,-250,0)
 elif user_hour==21 : sea(-750,750,-200,0)
 
+if user_hour==17 or user_hour==18 : beach(-750,750,-375,-300)
+elif user_hour==19 or user_hour==20 : beach(-750,750,-375,-250)
+elif user_hour==21 : beach(-750,750,-375,-200)
+
 if user_cloud==1 : cloud(-750,750,0,375)
 
 if user_hour==17 or user_hour==18 : fish(-750,750,-300,0)
@@ -30,9 +34,6 @@ elif user_hour==19 or user_hour==20 : waves(-750,750,-250,0)
 elif user_hour==21 : waves(-750,750,-200,0)
 # reflection()
 
-if user_hour==17 or user_hour==18 : beach(-750,750,-375,-300)
-elif user_hour==19 or user_hour==20 : beach(-750,750,-375,-250)
-elif user_hour==21 : beach(-750,750,-375,-200)
 
 bird(-750,750,0,375)
 
