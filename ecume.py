36ea651302f3539e1f82f7fa7f3da7c26f6af30d
Nora123-321 <nora.terral@gmail.com>
2024-11-03 15:56:17 +0100

    Create ecume.py

---
+++ ecume.py
@@ -0,0 +1,32 @@
+from modules import *
+
+def ecume():
+    """
+    Cette fonction permet de dessiner de l'écume
+    """
+    color("#F0F3F4")
+    begin_fill()
+    if user_hour==17 or user_hour==18 :
+        teleport(-750,-320)
+    elif user_hour==19 or user_hour==20 :
+        teleport(-750,-270)
+    else:
+        teleport(-750,-220)
+    y=ycor()
+    while xcor()<750:
+        if ycor()>=y+15:
+            goto(randint(xcor()+5,xcor()+10),randint(ycor()-2,ycor()))
+        elif ycor()<=y-10:
+            goto(randint(xcor()+5,xcor()+10),randint(ycor(),ycor()+2))
+        else:
+            goto(randint(xcor()+5,xcor()+10),randint(ycor()-2,ycor()+2))
+    goto(750,y+30)
+    y=ycor()
+    while xcor()> -750:
+        if ycor()>=y+15:
+            goto(randint(xcor()-10,xcor()-5),randint(ycor()-2,ycor()))
+        elif ycor()<=y-10:
+            goto(randint(xcor()-10,xcor()-5),randint(ycor(),ycor()+2))
+        else:
+            goto(randint(xcor()-10,xcor()-5),randint(ycor()-2,ycor()+2))
+    end_fill()
