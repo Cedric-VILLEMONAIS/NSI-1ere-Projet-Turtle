b0b14ad0d24040bd51880c218c861e88a27543f3
Nora123-321 <nora.terral@gmail.com>
2024-11-03 17:59:33 +0100

    Update reflection.py

--- reflection.py
+++ reflection.py
@@ -1,15 +1,56 @@
 from modules import *
 
-def reflection(x_start, x_end, y_start, y_end):
+def reflection():
     """
-    Cette fonction permet de représenter le reflet du soleil sur la mer
-    :param x_start: int
-    :param x_end: int
-    :param y_start: int
-    :param y_end: int
+    Cette fonction permet de dessiner le reflet du soleil sur l'eau
     """
     up()
-    goto(x,y)
     down()
-    color("") # créer variable soleil couleur pour la mettre la
-    fillcolor("")
+    if 17>=user_hour<18:
+        teleport(-230,-3)
+        color("#FCFE43")
+        y=-170
+        plus=100
+    elif 18>=user_hour<19:
+        teleport(-305,-3)
+        color("#FFD668")
+        y=-140
+        plus=70
+    elif 19>=user_hour<20:
+        teleport(-360,-3)
+        color("#FFCF68")
+        y=-100
+        plus=40
+    elif 20>=user_hour<21:
+        teleport(-430,-3)
+        color("#FFBF68")
+        y=-50
+        plus=20
+    else:
+        teleport(-512,-3)
+        color("#FF9668")
+        y=-30
+        plus=5
+    x=xcor()
+    begin_fill()
+    while ycor()>y:
+        if xcor()>=x+5:
+            goto(randint(xcor(),xcor()+2),randint(ycor()-6,ycor()-4))
+        elif xcor()<=x-5:
+            goto(randint(xcor()-2,xcor()),randint(ycor()-6,ycor()-4))
+        else:
+            goto(randint(xcor()-2,xcor()+2),randint(ycor()-6,ycor()-4))
+    y1=ycor()
+    x1=xcor()
+    teleport(x,-3)
+    goto(x+plus,-3)
+    x2=xcor()
+    while ycor()>y:
+        if xcor()>=x2+5:
+            goto(randint(xcor()-2,xcor()),randint(ycor()-6,ycor()-4))
+        elif xcor()<=x2-5:
+            goto(randint(xcor(),xcor()+2),randint(ycor()-6,ycor()-4))
+        else:
+            goto(randint(xcor()-2,xcor()+2),randint(ycor()-6,ycor()-4))
+    goto(x1,y1)
+    end_fill()
