59ceb76074c3b81683b8ac1ad08ef4e2fe4d1d2e
Nora123-321 <nora.terral@gmail.com>
2024-11-03 18:00:50 +0100

    Update sunset.py

--- sunset.py
+++ sunset.py
@@ -11,46 +11,49 @@ def sunset() :
     if 17>=user_hour<18:
         x=-80
         goto(x,y)
+        x=xcor()
         down()
         fillcolor("#FCFE43")
         begin_fill()
         color("#FCFE43")
         circle(100,180)
-        end_fill()
     elif 18>=user_hour<19:
         x=-200
         goto(x,y)
+        x=xcor()
         down()
         fillcolor("#FFD668")
         begin_fill()
         color("#FFD668")
         circle(70,180)
-        end_fill()
     elif 19>=user_hour<20:
         x=-300
         goto(x,y)
+        x=xcor()
         down()
         fillcolor("#FFCF68")
         begin_fill()
         color("#FFCF68")
         circle(40,180)
-        end_fill()
     elif 20>=user_hour<21:
         x=-400
         goto(x,y)
+        x=xcor()
         down()
         fillcolor("#FFBF68")
         begin_fill()
         color("#FFBF68")
         circle(20,180)
-        end_fill()
     else:
         x=-500
         goto(x,y)
+        x=xcor()
         down()
         fillcolor("#FF9668")
         begin_fill()
         color("#FF9668")
         circle(10,180)
-        end_fill()
+    goto(x,0)
+    end_fill()
         
+       
