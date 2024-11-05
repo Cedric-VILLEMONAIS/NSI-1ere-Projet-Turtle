2bae6cbc21c82eaa01af2c60b2e74729388e11cb
Nora123-321 <nora.terral@gmail.com>
2024-11-05 18:18:43 +0100

    Update ecume.py

--- ecume.py
+++ ecume.py
@@ -4,15 +4,15 @@ def ecume():
     """
     Cette fonction permet de dessiner de l'écume
     """
-    color("#F0F3F4")
-    begin_fill()
-    if user_hour==17 or user_hour==18 :
-        teleport(-750,-320)
-    elif user_hour==19 or user_hour==20 :
-        teleport(-750,-270)
-    else:
-        teleport(-750,-220)
-    y=ycor()
+    color("#F0F3F4") #on affecte du blanc comme couleur
+    begin_fill() #on active le mode remplissage
+    if user_hour==17 or user_hour==18 : #on réalise une condition: si l'heure choisie par l'utilisateur vaut soit 17, soit 18:
+        teleport(-750,-320) #on déplace la tortue vers les coordonnées x=-750 et y=-320 sans que le stylet ne dessine un trait entre sa position actuelle et sa future position
+    elif user_hour==19 or user_hour==20 : #on réalise une condition: si l'heure choisie par l'utilisateur vaut soit 19, soit 20:
+        teleport(-750,-270) #on déplace la tortue vers les coordonnées x=-750 et y=-320 sans que le stylet ne dessine un trait entre sa position actuelle et sa future position
+    else: #on réalise la fin de la condition: sinon:
+        teleport(-750,-220) #on déplace la tortue vers les coordonnées x=-750 et y=-320 sans que le stylet ne dessine un trait entre sa position actuelle et sa future position
+    y=ycor() #on
     while xcor()<750:
         if ycor()>=y+15:
             goto(randint(xcor()+5,xcor()+10),randint(ycor()-2,ycor()))
