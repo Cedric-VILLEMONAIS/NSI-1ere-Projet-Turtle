8e53371bee3ebb0804b9de84739d14aa466e4e9e
Nora123-321 <nora.terral@gmail.com>
2024-11-05 19:27:36 +0100

    Update fish.py

--- fish.py
+++ fish.py
@@ -1,6 +1,6 @@
 from modules import *
 
-def fish(x_start, x_end, y_start, y_end):
+def fish(x_start, x_end, y_start, y_end): #on crée un fonction "fish" pour dessiner des poissons
     """
     Cette fonction permet de créer des poissons
     def factorielle(n):
@@ -9,22 +9,22 @@ def fish(x_start, x_end, y_start, y_end):
     :param y_start: int
     :param y_end: int
     """
-    number = randint(5,10)
-    penup()
-    for i in range(number) :
-        width = randint(1,3)
-        up()
-        teleport(randint(x_start, x_end), randint(y_start, y_end))
-        pencolor("#646969")
-        fillcolor("#646969")
-        down()
-        setheading(randint(0,360))
-        dot(randint(4,6))
-        begin_fill()
-        right(60)
-        forward(6)
-        left(120)
-        forward(6)
-        left(120)
-        forward(6)
-        end_fill()
+    number = randint(5,10) #on créer la variable "number" à laquelle on affecte un nombre choisit aléatoirement entre 5 et 10
+    penup() #on relève le stylet
+    for i in range(number) : #on créer la boucle for qui va s'exécuter number fois, c'est-à-dire, le nombre aléatoire entre 5 et 10 affecter à la variable "number"
+        width = randint(1,3) #on modifie l'épaisseur du stylet avec pour épaisseur un nombre aléatoire entre 1 et 3
+        up() #on relève le stylet
+        teleport(randint(x_start, x_end), randint(y_start, y_end)) #on déplace la tortue (sans tracer de trait) au point de coordonnées: le nombre sur l'axe des abcisses qui vaut un nombre aléatoire entre "x_start" et "x_end"; et le nombre sur l'axe des ordonnées qui vaut un nombre aléatoire entre y_start et y_end 
+        pencolor("#646969") #on modifie la couleur du trait du stylo à la couleur "gris"
+        fillcolor("#646969") #on sélectionne la couleur "gris" pour le remplissage 
+        down() #on baisse le stylet; le stylet est en position d'écriture
+        setheading(randint(0,360)) #on oriente la tortue à une direction aléatoire
+        dot(randint(4,6)) #on fait un point d'épaisseur d'un nombre chosit aléatoirement entre les nombres 4 et 6
+        begin_fill() #on active le mode remplissage
+        right(60) #on tourne la tortue vers la droite de 60 degrés
+        forward(6) #on avance la tortue de 6 pixels
+        left(120) #on tourne la tortue vers la gauche de 120 degrés 
+        forward(6) #on avance la tortue de 6 pixels
+        left(120) #on tourne la tortue vers la gauche de 120 degrés 
+        forward(6) #on avance la tortue de 6 pixels
+        end_fill() #on désactive le mode remplissage
