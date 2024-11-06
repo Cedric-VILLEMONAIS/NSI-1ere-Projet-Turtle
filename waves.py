dc744ea8fb7d5efd9c588e1feb73a1fe8f92f2ad
Nora123-321 <nora.terral@gmail.com>
2024-11-06 14:43:58 +0100

    Update waves.py

--- waves.py
+++ waves.py
@@ -1,6 +1,6 @@
 from modules import *
 
-def waves(x_start, x_end, y_start, y_end): 
+def waves(x_start, x_end, y_start, y_end): #on crée la fonction "waves()" pour dessiner des vagues sur la mer
     """
     Cette fonction permet de créer des vagues
     def factorielle(n):
@@ -9,13 +9,12 @@ def waves(x_start, x_end, y_start, y_end):
     :param y_start: int
     :param y_end: int
     """
-    number = randint(20,50)
-    penup()
-    for i in range(number) :
-        width = randint(6,10)
-        up()
-        teleport(randint(x_start, x_end), randint(y_start, y_end))
-        pencolor("#154360")
+    number = randint(20,50) #on choisit un numéro au hasard entre 20 et 50 que l'on affecte dans la variable "number" que l'on crée au même moment
+    for i in range(number) : #on fait le programme autant de fois que la valeur de la variable "number"
+        width = randint(6,10) #on fait choisir au programme un nombre aléatoire entre 6 et 10 pour changer l'épaisseur du trait 
+        up() #on lève le stylet
+        teleport(randint(x_start, x_end), randint(y_start, y_end)) #
+        pencolor("#154360") #
         down()
         setheading(135)
         forward(6)
