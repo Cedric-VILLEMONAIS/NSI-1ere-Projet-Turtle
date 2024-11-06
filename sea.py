50090a37061ecef19b8cc3199cd91a88d0f36c71
Nora123-321 <nora.terral@gmail.com>
2024-11-06 14:10:57 +0100

    Update sea.py

--- sea.py
+++ sea.py
@@ -1,6 +1,6 @@
 from modules import *
 
-def sea(x_start, x_end, y_start, y_end) :
+def sea(x_start, x_end, y_start, y_end) : #on crée une fonction "sea()" pour dessiner la mer
     """
     cette fonction permet de faire apparaître la plage
     :param x_start: int
@@ -8,12 +8,12 @@ def sea(x_start, x_end, y_start, y_end) :
     :param y_start: int
     :param y_end: int
     """
-    teleport(x_start,y_start)
-    color("#21618C")
-    fillcolor("#21618C")
-    begin_fill()
-    goto(x_end, y_start)
-    goto(x_end,y_end)
-    goto(x_start,y_end)
-    goto(x_start, y_start)
-    end_fill()
+    teleport(x_start,y_start) #on déplace la tortue, en interrompant le dessin pour cette action, au point d'abcisse "x_start" (défini au moment de l'exécution du programme) et d'ordonnée "y_start" (défini au moment de l'exécution du programme)
+    color("#21618C") #on sélectionne la couleur bleu foncé pour les traits
+    fillcolor("#21618C") #on sélectione également la couleur bleu foncé pour le mode remplissage
+    begin_fill() #on active le mode remplissage
+    goto(x_end, y_start) #on déplace la tortue sans interrompre le dessin au point d'abcisse "x_end" (défini au moment de l'exécution du programme) et d'ordonnée "y_start" (défini au moment de l'exécution du programme)
+    goto(x_end,y_end) #on déplace la tortue sans interrompre le dessin au point d'abcisse "x_end" (défini au moment de l'exécution du programme) et d'ordonnée "y_end" (défini au moment de l'exécution du programme)
+    goto(x_start,y_end) #on déplace la tortue sans interrompre le dessin au point d'abcisse "x_start" (défini au moment de l'exécution du programme) et d'ordonnée "y_end" (défini au moment de l'exécution du programme)
+    goto(x_start, y_start) #on déplace la tortue sans interrompre le dessin au point d'abcisse "x_start" (défini au moment de l'exécution du programme) et d'ordonnée "y_start" (défini au moment de l'exécution du programme)
+    end_fill() #on désactive le mode remplissage
