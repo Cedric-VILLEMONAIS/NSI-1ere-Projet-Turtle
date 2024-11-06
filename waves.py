6099ef117bc799832a8a3113b1dfe0be9debc030
Nora123-321 <nora.terral@gmail.com>
2024-11-06 14:53:12 +0100

    Update waves.py

--- waves.py
+++ waves.py
@@ -13,11 +13,11 @@ def waves(x_start, x_end, y_start, y_end): #on crée la fonction "waves()" pour 
     for i in range(number) : #on fait le programme autant de fois que la valeur de la variable "number"
         width = randint(6,10) #on fait choisir au programme un nombre aléatoire entre 6 et 10 pour changer l'épaisseur du trait 
         up() #on lève le stylet
-        teleport(randint(x_start, x_end), randint(y_start, y_end)) #
-        pencolor("#154360") #
-        down()
-        setheading(135)
-        forward(6)
-        circle(10,100)
-        setheading(205)
-        forward(6)
+        teleport(randint(x_start, x_end), randint(y_start, y_end)) #on déplace la tortue (sans tracer de trait) au point de coordonnées: le nombre sur l'axe des abcisses qui vaut un nombre aléatoire entre "x_start" et "x_end" qui sont définis à l'exécution de programme; et le nombre sur l'axe des ordonnées qui vaut un nombre aléatoire entre y_start et y_end qui sont définis à l'exécution du programme 
+        pencolor("#154360") #on sélectionne la couleur bleu marine aux traits du stylet 
+        down() #on baisse le stylet pour pouvoir modifier le dessin
+        setheading(135) #on oriente la tortue à 135 degrés
+        forward(6) #on fait avancer la tortue de 6 pixels
+        circle(10,100) #on dessine un ar-de-cercle de 10 pixels de rayon et de 100 degrés
+        setheading(205) #on oriente la totue à 205 degrés
+        forward(6) #on fait avancer la tortue de 6 pixels
